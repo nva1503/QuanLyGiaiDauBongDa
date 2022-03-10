778a9bbc458ec83615de54720f5b5147a8f0f6bc
tung <tungnthe150190@fpt.edu.vn>
2022-03-11 02:01:41 +0700

    demo

--- QuanLyGiaiDauBongDa/FrmAboutUs.Designer.cs
+++ QuanLyGiaiDauBongDa/FrmAboutUs.Designer.cs
@@ -101,6 +101,7 @@ namespace QuanLyGiaiDauBongDa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(809, 486);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox5);
